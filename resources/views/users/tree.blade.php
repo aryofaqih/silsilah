9b045c6bd6cf59d9ae3b2721da7cc4d93fa7164c
aryofaqih <aryofaqih@gmail.com>
2024-02-04 22:02:11 +0700

    pohon keluarga dengan 7 keturunan

--- resources/views/users/tree.blade.php
+++ resources/views/users/tree.blade.php
@@ -10,6 +10,8 @@ $grandChildsTotal = 0;
 $ggTotal = 0;
 $ggcTotal = 0;
 $ggccTotal = 0;
+$lv6Total = 0;
+$lv7Total = 0;
 ?>
 
 <div id="wrapper">
@@ -44,6 +46,26 @@ $ggccTotal = 0;
                                             @foreach($ggc->childs as $ggcc)
                                             <div class="entry {{ $ggccCount == 1 ? 'sole' : '' }}">
                                                 <span class="label">{{ link_to_route('users.tree', $ggcc->name, [$ggcc->id], ['title' => $ggcc->name.' ('.$ggcc->gender.')']) }}</span>
+                                                @if ($lv6Count = $ggcc->childs->count())
+                                                <?php $lv6Total += $lv6Count ?>
+                                                <div class="branch lv6">
+                                                    @foreach($ggcc->childs as $ggcclv6)
+                                                    <div class="entry {{ $lv6Count == 1 ? 'sole' : '' }}">
+                                                        <span class="label">{{ link_to_route('users.tree', $ggcclv6->name, [$ggcclv6->id], ['title' => $ggcclv6->name.' ('.$ggcclv6->gender.')']) }}</span>
+                                                        @if ($lv7Count = $ggcclv6->childs->count())
+                                                        <?php $lv7Total += $lv7Count ?>
+                                                        <div class="branch lv7">
+                                                            @foreach($ggcclv6->childs as $ggcclv7)
+                                                            <div class="entry {{ $lv6Count == 1 ? 'sole' : '' }}">
+                                                                <span class="label">{{ link_to_route('users.tree', $ggcclv7->name, [$ggcclv7->id], ['title' => $ggcclv7->name.' ('.$ggcclv7->gender.')']) }}</span>
+                                                            </div>
+                                                            @endforeach
+                                                        </div>
+                                                        @endif
+                                                    </div>
+                                                    @endforeach
+                                                </div>
+                                                @endif
                                             </div>
                                             @endforeach
                                         </div>
@@ -69,28 +91,27 @@ $ggccTotal = 0;
 <div class="container">
 <hr>
 <div class="row">
-    <div class="col-md-1">&nbsp;</div>
     @if ($childsTotal)
-    <div class="col-md-1 text-right">{{ trans('app.child_count') }}</div>
-    <div class="col-md-1 text-left"><strong style="font-size:30px">{{ $childsTotal }}</strong></div>
+    <div class="col-md-1 text-right">{{ trans('app.child_count') }} <br><strong style="font-size:30px">{{ $childsTotal }}</strong></div>
     @endif
     @if ($grandChildsTotal)
-    <div class="col-md-1 text-right">{{ trans('app.grand_child_count') }}</div>
-    <div class="col-md-1 text-left"><strong style="font-size:30px">{{ $grandChildsTotal }}</strong></div>
+    <div class="col-md-1 text-right">{{ trans('app.grand_child_count') }} <br><strong style="font-size:30px">{{ $grandChildsTotal }}</strong></div>
     @endif
     @if ($ggTotal)
-    <div class="col-md-1 text-right">Jumlah Cicit</div>
-    <div class="col-md-1 text-left"><strong style="font-size:30px">{{ $ggTotal }}</strong></div>
+    <div class="col-md-1 text-right">Jumlah Cicit <br><strong style="font-size:30px">{{ $ggTotal }}</strong></div>
     @endif
     @if ($ggcTotal)
-    <div class="col-md-1 text-right">Jumlah Keturunan ke 4</div>
-    <div class="col-md-1 text-left"><strong style="font-size:30px">{{ $ggcTotal }}</strong></div>
+    <div class="col-md-1 text-right">Keturunan ke 4 <br><strong style="font-size:30px">{{ $ggcTotal }}</strong></div>
     @endif
     @if ($ggccTotal)
-    <div class="col-md-1 text-right">Jumlah Keturunan ke 5</div>
-    <div class="col-md-1 text-left"><strong style="font-size:30px">{{ $ggccTotal }}</strong></div>
+    <div class="col-md-1 text-right">Keturunan ke 5 <br><strong style="font-size:30px">{{ $ggccTotal }}</strong></div>
     @endif
-    <div class="col-md-1">&nbsp;</div>
+    @if ($lv6Total)
+    <div class="col-md-1 text-right">Keturunan ke 6 <br><strong style="font-size:30px">{{ $lv6Total }}</strong></div>
+    @endif
+    @if ($lv7Total)
+    <div class="col-md-1 text-right">Keturunan ke 7 <br><strong style="font-size:30px">{{ $lv7Total }}</strong></div>
+    @endif
 </div>
 @endsection
 
